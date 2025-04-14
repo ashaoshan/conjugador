c3f5e272cf233bef7ba8d72c4bdabfe4cbe8a76e
ashaoshan <ashaoshan@gmail.com>
2025-04-14 20:01:39 +0300

    Update openai-conjugador.js

--- pages/api/openai-conjugador.js
+++ pages/api/openai-conjugador.js
@@ -8,6 +8,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function handler(req, res) {
+  console.log("CLAVE DE API:", process.env.OPENAI_API_KEY);
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Método no permitido" });
   }
